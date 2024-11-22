3d8b3548060b9f1e4d43a59330648709fb03d391
DogBrother <roys99141@gmail.com>
2024-11-22 17:59:57 +0800

    ejs 真偉大

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const ejs = require('ejs'); /*導入ejs*/
 const app = express();
 
+/*設定 view engine 為 ejs*/
+app.set('view engine', 'ejs');
+
 app.get('/',(req,res)=>{
-    res.send("HEllo WOrld");
+    /*渲染畫面  並將後面參數傳置檔案*/
+    res.render('index', {
+        game: 'Final Fantasy VII',
+        category: '<p><b>Characters:</b></p>',
+        characters: ['Cloud', 'Aerith', 'Tifa', 'Barret']
+    });
 });
 
 
