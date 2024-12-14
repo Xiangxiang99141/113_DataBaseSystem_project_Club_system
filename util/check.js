07cda1dab1a20670798a1982505e2045dd281775
DogBrother <roys99141@gmail.com>
2024-12-15 00:56:03 +0800

    add check

---
+++ util/check.js
@@ -0,0 +1,10 @@
+module.exports = class Check{
+
+    //判斷email格式
+    checkEmail(email){
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const result = re.test(email);
+        return result;
+    }
+
+}
