0bb932d755bdbc156a512b40b058a94f471bfbf9
DogBrother <roys99141@gmail.com>
2024-12-15 10:14:17 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const ejs = require('ejs'); /*導入ejs*/
-// const bodyParser = require('body-parser'); // 導入Body-Parser
 const path = require('path');
 const app = express();
 
 
 //Routes
-const indexRouter = require('./routes/indexRouter');
+// const indexRouter = require('./routes/indexRouter');
+const memberRouter = require('./routes/memberRouter')
 
 /*設定 view engine 為 ejs*/
 app.set('view engine', 'ejs');
@@ -24,7 +24,8 @@ app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js
 app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')));
 
 //設定路由
-app.use('/',indexRouter);
+// app.use('/',indexRouter);
+app.use('/user',memberRouter);
 
 //製作API路由
 // app.use('/api');
