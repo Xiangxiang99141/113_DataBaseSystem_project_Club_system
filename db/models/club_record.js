af96fa82c98130379d2c5a2288f46303dcb366d4
DogBrother <roys99141@gmail.com>
2024-12-17 10:54:59 +0800

    add record model

---
+++ db/models/club_record.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Club_record extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Club_record.init({
+    Cr_id: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    M_id: {
+      type:DataTypes.UUID,
+      allowNull:false
+    },
+    Cr_type: {
+      type:DataTypes.ENUM('社課','社團活動'),
+      allowNull:false
+    },
+    Ca_id: {
+      type:DataTypes.INTEGER
+    },
+    Cc_id: {
+      type:DataTypes.INTEGER
+    },
+    Cr_comment: {
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    Cr_vote: {
+      type:DataTypes.ENUM('非常滿意','滿意','尚可','不滿意','非常不滿意'),
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Club_record',
+    tableName: 'Club_records',
+    timestamps:false,
+    updatedAt:false,
+    createdAt:false
+  });
+  return Club_record;
+};
