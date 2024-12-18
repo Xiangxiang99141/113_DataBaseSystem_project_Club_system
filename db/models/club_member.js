6171ca52535ed04e06139ab5aa49ea0c6cbab8e7
DogBrother <roys99141@gmail.com>
2024-12-18 17:55:22 +0800

    update club_member model

--- db/models/club_member.js
+++ db/models/club_member.js
@@ -12,33 +12,40 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       //定義關聯
+      Club_member.belongsTo(models.Member,{
+        foreignKey:"M_id"
+      });
     }
   }
   Club_member.init({
     C_id: {
-      type:DataTypes.BIGINT,
-      allowNull:false
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true
     },
     M_id: {
-      type:DataTypes.UUID,
-      allowNull:false
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      references: {
+        model: 'Members',
+        key: 'M_id'
+      }
     },
     Cme_job: {
-      type:DataTypes.ENUM('社長','副社長','幹部','社團指導老師','社員'),
-      allowNull:false
+      type: DataTypes.ENUM('社長','副社長','幹部','社團指導老師','社員'),
+      allowNull: false
     },
     Cme_member_join_at: {
-      type:DataTypes.DATE,
-      allowNull:false,
-      defaultValue:new Date()
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
     }
   }, {
     sequelize,
     modelName: 'Club_member',
-    tableNameName: 'Club_members',
-    timestamps:false,
-    createdAt:false,
-    updatedAt:false
+    tableName: 'Club_members',
+    timestamps: false
   });
   return Club_member;
 };
