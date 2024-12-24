1b678b2bb532af50516312acf673b2e8579ddca7
DogBrother <roys99141@gmail.com>
2024-12-24 11:25:27 +0800

    Update transportation.js
    更新關聯

--- db/models/transportation.js
+++ db/models/transportation.js
@@ -11,7 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Transportation.belongsTo(models.signup_record);
+      Transportation.hasOne(models.Signup_record, {
+        foreignKey: "Ts_id"
+      });
     }
   }
   Transportation.init({
