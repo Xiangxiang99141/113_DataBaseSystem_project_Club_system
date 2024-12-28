e739e2c9beda9f6ebb6d24eed74d4342c05a468f
DogBrother <roys99141@gmail.com>
2024-12-28 15:44:26 +0800

    Create club_equipment.js
    新增model

---
+++ db/models/club_equipment.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Club_equipment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Club__equipment.belongsTo(models.Club,{
+        foreignKey:'C_id'
+      });
+      Club__equipment.belongsTo(models.Member,{
+        foreignKey:'Ce_admin'
+      });
+      Club__equipment.belongsTo(models.Member,{
+        foreignKey:'Ce_report'
+      });
+    }
+  }
+  Club_equipment.init({
+    Ce_id: {
+      allownull:false,
+      primaryKey:true,
+      autoIncrement:true,
+      type:DataTypes.INTEGER,
+    },
+    Ce_name: {
+      type:DataTypes.STRING(10),
+      allownull:false
+    },
+    Ce_spec: {
+      type:DataTypes.STRING(20),
+      allownull:false
+    },
+    Ce_count: {
+      type:DataTypes.INTEGER,
+      allownull:false
+    },
+    Ce_use: {
+      type:DataTypes.STRING(20),
+      allownull:false
+    },
+    Ce_source: {
+      type:DataTypes.ENUM('社團自籌','學校經費','贊助'),
+      allownull:false
+    },
+    Ce_img: {
+      type:DataTypes.STRING
+    },
+    Ce_admin: {
+      type:DataTypes.UUID,
+      allownull:false
+    },
+    Ce_report: {
+      type:DataTypes.UUID,
+      allownull:false
+    },
+    Ce_purch_at: {
+      type:DataTypes.DATEONLY,
+      allownull:false
+    },
+    C_id: {
+      type:DataTypes.BIGINT,
+      allownull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Club_equipment',timestamps:false,
+    tableName:'Club__equipments'
+  });
+  return Club_equipment;
+};
