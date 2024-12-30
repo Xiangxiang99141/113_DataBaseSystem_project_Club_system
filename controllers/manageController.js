2b1a7144ef86b7d6f696276fa983a1466f67ba82
DogBrother <roys99141@gmail.com>
2024-12-31 00:13:49 +0800

    Update manageController.js
    將獲取token內容獨立為額外檔案
    新增依照params獲取社團各項資訊

--- controllers/manageController.js
+++ controllers/manageController.js
@@ -1,21 +1,21 @@
 const { Club_member, Club_activity, Club_course, Club_meeting } = require('../db/models');
 const {Op} = require('sequelize')
-const verification = require('../util/verification');
+// const verification = require('../util/verification');
+const util = require('../util/util');
 const COOKIE_NAME = 'auth_token';
 
 
 exports.getview = (req,res) => {
     
-    const token = req.cookies[COOKIE_NAME];
-    if(token){
-        verification(token).then(user=>{
-            console.log(user);
-        })
-    };
-
+    // const token = req.cookies[COOKIE_NAME];
+    // if(token){
+    //     verification(token).then(user=>{
+    //     })
+    // };
+    const islogin = util.loginInfo(req.cookies[COOKIE_NAME]);
 
     try{
-        HasAdmin().then(result=>{
+        HasAdmin(req.query.id).then(result=>{
             res.render('manage/Admin_index',result);
         });
     }catch(error){
@@ -29,25 +29,30 @@ exports.getview = (req,res) => {
 
 
 
-async function HasAdmin(){
+async function HasAdmin(id){
     const currentDate = new Date();
     const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
     const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-
     // 獲取統計數據
-    const memberCount = await Club_member.count();
+    const memberCount = await Club_member.count({
+        where: {
+            C_id:id
+        }
+    });
     const activityCount = await Club_activity.count({
         where: {
             Ca_date: {
                 [Op.between]: [firstDayOfMonth, lastDayOfMonth]
-            }
+            },
+            C_id:id
         }
     });
     const courseCount = await Club_course.count({
         where: {
             Cc_date: {
                 [Op.between]: [firstDayOfMonth, lastDayOfMonth]
-            }
+            },
+            C_id:id
         }
     });
     return {
