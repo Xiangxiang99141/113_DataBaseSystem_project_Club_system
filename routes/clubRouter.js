23f5539e77e1b128b2cd6845265838420b673efe
DogBrother <roys99141@gmail.com>
2024-12-31 10:06:57 +0800

    Update clubRouter.js
    新增創建社團路由

--- routes/clubRouter.js
+++ routes/clubRouter.js
@@ -2,8 +2,40 @@ const express = require('express');
 const router = express.Router();
 const clubController = require('../controllers/clubController');
 const { isAuthenticated } = require('../middleware/auth');
+const verification = require('../util/verification');
+const { error } = require('jquery');
+const COOKIE_NAME = 'auth_token';
+
 
 // 創建社團
+router.get('/',async (req,res)=>{
+    let is_login = false;
+    let user = null;
+    switch(req.query.action){
+        case 'create':
+            if(req.cookies[COOKIE_NAME]){
+                const token = req.cookies[COOKIE_NAME];
+                user = await verification(token);
+                if(user){
+                    is_login = true
+                }
+                res.render('create-club',{
+                isLogin:is_login,
+                error:null,
+                success:null
+            });
+            }else{
+                res.redirect('/login');
+            }
+            break;
+        case 'info':
+            break;
+        default:
+            break;
+        }
+
+});
+
 router.post('/create', clubController.createClub);
 
 // 獲取社團列表
