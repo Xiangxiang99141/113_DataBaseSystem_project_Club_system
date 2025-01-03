ad316c4a4d137ff0590e6820ac5570a5cf9fcee3
DogBrother <roys99141@gmail.com>
2025-01-03 17:43:35 +0800

    Update clubRouter.js
    移除無用路由

--- routes/clubRouter.js
+++ routes/clubRouter.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const clubController = require('../controllers/clubController');
 const { isAuthenticated } = require('../middleware/auth');
-const verification = require('../util/verification');
-const COOKIE_NAME = 'auth_token';
 
 
 //獲取社團路由首頁
@@ -14,12 +12,6 @@ router.get('/info',clubController.getInfoHome);
 
 //獲取社團資訊
 router.get('/info/:id',clubController.getInfo);
-
-
-router.post('/create', clubController.createClub);
-
-// 獲取社團列表
-router.get('/list', clubController.getClubs);
 
 // API: 報名社團（需要登入）
 router.post('/api/clubs/apply', isAuthenticated, clubController.applyClub);
