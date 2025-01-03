b4eb140f7cd30af52449b6c3713ed1e8fc6e2d91
DogBrother <roys99141@gmail.com>
2025-01-03 16:16:40 +0800

    Update indexRouter.js
    修正已登入還可以進入登入頁面

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const verification = require('../util/verification');
 
 const indexController = require('../controllers/indexController');
 
@@ -9,12 +9,24 @@ router.get('/', indexController.Renderindex);
 
 // 登入頁面
 router.get('/login', (req, res) => {
-    res.render('login', { error: req.flash('error') });
+    if(req.cookies['auth_token']){
+        const user = verification(req.cookies['auth_token']);
+        console.log(user);
+        if(user){
+            res.redirect('/');
+            return;
+        }else{
+            res.render('login', { error: req.flash('error','認證失敗，請重新登入') });
+        }
+        
+    }else{
+        res.render('login', { error: req.flash('error') });
+    }
 });
 
 // 註冊頁面
 router.get('/signup', (req, res) => {
-    res.render('signup', { error: false });
+        res.render('signup', { error: false });
 });
 
 module.exports = router;
