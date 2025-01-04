3a0c85a2ffb9dac2f4f9982b2f04ac005576f165
DogBrother <roys99141@gmail.com>
2025-01-04 17:23:45 +0800

    Create member.js
    社員名單script

---
+++ public/script/member.js
@@ -0,0 +1,47 @@
+// 獲取成員列表
+function getmemberlist(){
+    fetch('/api/members',{
+        method:'GET',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }).then(response=>{
+        if(response.ok){
+            response.json().then(data=>{
+                const select = document.getElementById('userSelect');
+                data.data.forEach(member => {
+                    option = document.createElement('option');
+                    option.value = member.M_id;
+                    option.text = member.M_name;
+                    select.add(option);
+                });
+                new bootstrap.Modal(document.getElementById('addMemberModal')).show();
+            });
+        }
+    })
+}
+function addMember(id){
+    let formDataObj = {};
+    const form = document.getElementById('addMemberForm');
+    const formData = new FormData(form);
+    formData.forEach((value,key)=>{
+        formDataObj[key] = value;
+    });
+    fetch(`/api/club/${id}/member`,{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(formDataObj)
+    }).then(response=>{
+        if(response.ok){
+            response.json().then(data=>{
+                if(data.success){
+                    window.location.reload();
+                }else{
+                    alert(data.message);
+                }
+            });
+        }
+    });
+}
