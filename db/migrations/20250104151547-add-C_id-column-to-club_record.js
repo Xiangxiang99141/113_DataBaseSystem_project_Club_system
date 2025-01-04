be580c7c409898e26b017c59721b0ee70cc764b4
DogBrother <roys99141@gmail.com>
2025-01-04 23:20:19 +0800

    Create 20250104151547-add-C_id-column-to-club_record.js
    應該還是要有個社團 id欄位比較好篩選

---
+++ db/migrations/20250104151547-add-C_id-column-to-club_record.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Club_records', 'C_id', {
+      type: Sequelize.BIGINT,
+      allowNull: false,
+      references: {
+        model: 'Clubs',
+        key: 'C_id'
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Club_records', 'C_id');
+  }
+};
