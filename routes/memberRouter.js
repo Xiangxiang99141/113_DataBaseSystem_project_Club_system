954b48fae59a1ebd80259c5af70bd80faa3e4af8
DogBrother <roys99141@gmail.com>
2025-01-07 00:10:50 +0800

    Update memberRouter.js
    移除
    - 移除無用路由

--- routes/memberRouter.js
+++ routes/memberRouter.js
@@ -6,7 +6,6 @@ const { isAuthenticated } = require('../middleware/auth');
 // API 路由
 router.post('/register', memberController.createUser);
 router.post('/login', memberController.login);
-router.post('/login',isAuthenticated, memberController.login);
 router.get('/logout', isAuthenticated, memberController.logout);
 router.put('/update', isAuthenticated, memberController.update);
 
