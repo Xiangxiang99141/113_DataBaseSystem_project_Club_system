4ee7d607ad408bfb084de051466efb6687501bf5
DogBrother <roys99141@gmail.com>
2025-01-07 00:09:47 +0800

    Update indexRouter.js
    修正
    - 將各項功能移至controller

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,28 +5,12 @@ const verification = require('../util/verification');
 const indexController = require('../controllers/indexController');
 
 // 首頁顯示社團列表
-router.get('/', indexController.Renderindex);
+router.get('/', indexController.renderIndex);
 
 // 登入頁面
-router.get('/login', (req, res) => {
-    if(req.cookies['auth_token']){
-        const user = verification(req.cookies['auth_token']);
-        console.log(user);
-        if(user){
-            res.redirect('/');
-            return;
-        }else{
-            res.render('login', { error: req.flash('error','認證失敗，請重新登入') });
-        }
-        
-    }else{
-        res.render('login', { error: req.flash('error') });
-    }
-});
+router.get('/login', indexController.loginView);
 
 // 註冊頁面
-router.get('/signup', (req, res) => {
-        res.render('signup', { error: false });
-});
+router.get('/signup', indexController.signupView);
 
 module.exports = router;
